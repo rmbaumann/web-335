0c905aecf799545857ecbd9ceb27e0bead053599
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-08-19 19:04:06 -0500

    Create baumann-calculator.py

---
+++ week-8/baumann-calculator.py
@@ -0,0 +1,38 @@
+# ;============================================
+# ; Title: Exercise 8.3
+# ; Author: Reva Baumann
+# ; Date: 08/19/2019
+# ; Modified By: Reva Baumann
+# ; Description: Baumann Calculator Python
+# ;===========================================
+
+# Start Program
+
+# Output expectations:
+# 3
+# 3
+# 4.0
+
+#Header
+first_name = "Reva"
+last_name = "Baumann"
+print(first_name + ' ' + last_name)
+
+# Addition
+def add(a, b):
+    return a + b
+
+# Subtraction
+def subtract(a, b):
+    return a - b
+
+# Division
+def divide(a, b):
+    return a / b
+
+# print the values of the equations
+print(add(1, 2))
+print(subtract(4, 1))
+print(divide(8, 2))
+
+#End Program
