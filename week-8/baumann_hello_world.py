30cbc5bc652894c9adb16672a117d1ceda5da68c
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-08-19 19:04:09 -0500

    Update baumann_hello_world.py

--- week-8/baumann_hello_world.py
+++ week-8/baumann_hello_world.py
@@ -1,1 +1,9 @@
+# ;============================================
+# ; Title: Exercise 8.2
+# ; Author: Reva Baumann
+# ; Date: 08/19/2019
+# ; Modified By: Reva Baumann
+# ; Description: Anaconda - Hello World
+# ;===========================================
+
 print('Hello World')
